28db204d68d8f9822e5a51f886d5c97e311fc0d2
saumyasahu-7 <saumyasahu.7feb@gmail.com>
2024-07-14 21:06:46 +0530

    Created encapsulation folder

---
+++ encapsulation/name_mangling.py
@@ -0,0 +1,10 @@
+# Python program to demonstrate 
+# name mangling 
+
+class Student: 
+	def __init__(self, name): 
+		self.__name = name 
+
+s1 = Student("Saumya") 
+print(dir(s1)) 
+print(s1._Student__name) 
